7182829db2b7dbba99ba0633f893077d4d0d586d
Brian Faherty <bfaherty@fool.com>
2014-09-04 14:57:51 -0400

    Back to development: 0.0.6.3

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-version = '0.0.6.2'
+version = '0.0.6.3.dev0'
 install_requires = ['pytest', 'django-model-utils >= 2.0.2', 'requests', ]
 dependency_links = ["http://localshop.foolhq.com/simple"]
 

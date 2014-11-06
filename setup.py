df8a44ce6be793bb956ed97ea0145b04692767d8
Chris Thatcher <cthatcher@fool.com>
2014-11-06 15:16:39 -0500

    Versioning for development 0.0.7.0.dev0

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-version = '0.0.7.0'
+version = '0.0.7.0.dev0'
 install_requires = ['pytest', 'django-model-utils >= 2.0.2', 'requests', ]
 dependency_links = ["http://localshop.foolhq.com/simple"]
 
